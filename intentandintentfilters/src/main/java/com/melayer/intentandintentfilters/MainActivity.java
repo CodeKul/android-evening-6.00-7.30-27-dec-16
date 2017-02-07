f356ca4f297c7459ce546dd8cbd9aa642ba9d7c4
aniruddha <aniruddha.kudalkar@gmail.com>
2017-02-07 19:58:28 +0530

    intents and intent filters

---
+++ intentandintentfilters/src/main/java/com/melayer/intentandintentfilters/MainActivity.java
@@ -0,0 +1,36 @@
+package com.melayer.intentandintentfilters;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnNext).setOnClickListener(this::call);
+    }
+
+    private void goNext(View view) {
+        Intent intent = new Intent("com.codekul.intent.action.SPORTS");
+        intent.addCategory("com.codekul.category.TENNIS");
+        intent.setData(Uri.parse("tel://9897"));
+        startActivity(intent);
+    }
+
+    private void dial(View v) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        startActivity(intent);
+    }
+
+    private void call(View view) {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel://9762548833"));
+        startActivity(intent);
+    }
+}
